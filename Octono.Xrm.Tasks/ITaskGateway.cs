bfa3f2fe1c46b9a31d89b85eab6793d887abefde
Darren Lewis <darren@octono.com>
2014-10-27 11:23:32 +0000

    Removed unused files

--- Octono.Xrm.Tasks/ITaskGateway.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xrm.Sdk;
-
-namespace Octono.Xrm.Tasks
-{
-    public interface ITaskGateway
-    {
-        Entity RetrieveWebResource(IOrganizationService service, string name);
-    }
-}
