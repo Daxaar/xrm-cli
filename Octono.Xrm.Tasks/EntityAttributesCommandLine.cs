6f7f678e93f39fab18da16563f99f00f23b42775
Darren Lewis <darren@octono.com>
2017-12-08 11:19:05 +0000

    Added EntityAttributesTask

---
+++ Octono.Xrm.Tasks/EntityAttributesCommandLine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Octono.Xrm.Tasks.Utils;
+
+namespace Octono.Xrm.Tasks
+{
+    public class EntityAttributesCommandLine : CommandLine
+    {
+        public EntityAttributesCommandLine(IList<string> args) : base(args)
+        {
+            EntityName = args[1];
+
+            Filter = args.ArgumentValue("-f");
+        }
+
+        public string Filter { get; set; }
+
+        public string EntityName { get; set; }
+    }
+}
---
+++ Octono.Xrm.Tasks/EntityAttributesTask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Octono.Xrm.Tasks.Utils;
+
+namespace Octono.Xrm.Tasks
+{
+    /// <summary>
+    /// Lists the attribute names of an entity
+    /// </summary>
+    [XrmTask(typeof(EntityAttributesCommandLine), "Attributes", Aliases = new[] {"attr","attributes"})]
+    public class EntityAttributesTask : IXrmTask
+    {
+        private readonly EntityAttributesCommandLine _cmdLine;
+
+        public EntityAttributesTask(EntityAttributesCommandLine cmdLine)
+        {
+            _cmdLine = cmdLine;
+        }
+        public void Execute(IXrmTaskContext context)
+        {
+            var service = context.ServiceFactory.Create(_cmdLine.ConnectionName);
+
+            var req = new RetrieveEntityRequest
+            {
+                EntityFilters = EntityFilters.Attributes,
+                LogicalName = _cmdLine.EntityName
+            };
+
+            var response  = service.Execute(req);
+
+            var attributes = response.Results.Values.SelectMany(x =>
+                    ((EntityMetadata) x).Attributes
+                    .Select(y => new {
+                        SchemaName = y.SchemaName + (y.IsPrimaryId == true ? " * (id)" : "") + (y.IsPrimaryName == true ? " * (name)" : ""),
+                        Type = y.AttributeTypeName.Value.Replace("Type","")})
+                    .OrderBy(a => a.SchemaName))
+                .ToList();
+
+            var longestSchemaName = attributes.Max(x => x.SchemaName.Length);
+            var longestTypeName = attributes.Max(x => x.Type.Length);
+            
+            Console.WriteLine("|" + string.Join("",Enumerable.Repeat("=", longestSchemaName + longestTypeName + 5)) + "|");
+            foreach (var attr in attributes.Where(x => x.SchemaName.Contains(_cmdLine.Filter)))
+            {
+                Console.WriteLine($"| {attr.SchemaName.PadRight(longestSchemaName)} | {attr.Type.PadRight(longestTypeName)} |");
+            }
+            Console.WriteLine("|" + string.Join("", Enumerable.Repeat("=", longestSchemaName + longestTypeName + 5)) + "|");
+        }
+    }
+}
